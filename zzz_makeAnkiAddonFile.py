42c1d568e02d1029d6d3fb682ef6453d35b7e311
wiz <76851476+wizpizz@users.noreply.github.com>
2025-12-06 16:27:08 +0100

    fix: addon name again

--- zzz_makeAnkiAddonFile.py
+++ zzz_makeAnkiAddonFile.py
@@ -3,7 +3,7 @@ import zipfile
 from datetime import datetime
 from _versions import __version__
 
-ADDON_NAME = f"Ankicord Customize by Shige {__version__}"
+ADDON_NAME = f"Ankicord Customized by Shige {__version__}"
 
 
 def create_ankiaddon():
